6af62cd148636214f415ac29bb89940c187e1238
Johannes Hammermann <hammermann@campus.tu-berlin.de>
2026-06-01 10:55:03 +0200

    ifelse

--- src/SparsityProbes.jl
+++ src/SparsityProbes.jl
@@ -37,6 +37,8 @@ module SparsityProbes
         for (j, val) in enumerate(x)
             if j in chunk
                 xt[j] = T(val)
+            else
+                xt[j] = nothing
             end
         end
         return xt
